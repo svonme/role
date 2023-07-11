c2cb14c446a38ecf3888ff2cdd2e3e6aea294c6b
svonme <svon.me@gmail.com>
2023-07-11 16:29:18 +0800

    doc: types 导出

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * as role from "./utils/role";
 export { MenuType } from "./utils/type";
-export type { Menu } from "./utils/type";
+export type { Menu, RoleType, RoleCallback } from "./utils/type";
 // 组件
 export { default as Role } from "./components/role";
 // 指令
